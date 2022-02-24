a08ec4cb7d3a66d762370098325d5d3e7c1c3f8a
Aure31880 <barbotin48@gmail.com>
2022-02-24 20:58:28 +0100

    fix password validator

--- backend/models/Password-validator.js
+++ backend/models/Password-validator.js
@@ -4,7 +4,6 @@ var passwordSchema = new passwordValidator();
 
 passwordSchema
     .is().min(8)
-    .has().digits(2)
     .has().not().spaces()
     .is().not().oneOf(['Password', 'Passw0rd', 'Password123', 'password']);
 
